8106076a91aeef1c30b56fbc62f22633b8bc7efb
jalil <jalilahmad1309@gmail.com>
2023-03-31 03:39:09 +0430

    Create serializers for Create-Delete-List flash card

---
+++ card/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from card import views
+
+urlpatterns = [
+    path('create/', views.CreateFlashCardView.as_view(), name="create-flash-card"),
+    path('update/<id>/', views.UpdateFlashCardView.as_view(), name="update-flash-card"),
+    path('dalete/<id>/', views.DeleteFlashCardView.as_view(), name="delete-flash-card"),
+    path('list/<user_id>/', views.ListFlashCardView.as_view(), name="list-user-flash-card"),
+]
+
