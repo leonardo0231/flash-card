2cc034131a0076bd67e63920417c7135cf46a0c0
jalil <jalilahmad1309@gmail.com>
2023-03-31 03:44:39 +0430

    Create view for flash cards

--- card/views.py
+++ card/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import status
+from card.models import FlashCard
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, get_list_or_404
+from card.serializers import (
+    CreateFlashCardSerializer,
+    UpdateFlashCardSerializer,
+    ListFlashCardSerializer,
+)
 
-# Create your views here.
+class CreateFlashCardView(APIView):
+    def post(self, request):
+        serializer = CreateFlashCardSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class UpdateFlashCardView(APIView):
+    def put(self, request, id):
+
+        flash_card = get_object_or_404(FlashCard, id=id)
+
+        serializer = UpdateFlashCardSerializer(
+            data=request.data,
+            instance=flash_card,
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class DeleteFlashCardView(APIView):
+
+    def delete(self, request, id):
+        flash_card = get_object_or_404(FlashCard, id=id)
+        flash_card.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class ListFlashCardView(APIView):
+    def get(self, request, user_id):
+        all_flash_card_user = get_list_or_404(FlashCard, user__id=user_id)
+        serializer = ListFlashCardSerializer(all_flash_card_user, many=True)
+
+        return Response(serializer.data)
