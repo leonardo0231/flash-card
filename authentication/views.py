f1ff3195b893818466d524fe3ce091ac3d50c667
jalil <jalilahmad1309@gmail.com>
2023-03-31 01:57:55 +0430

    Create api views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from authentication.serializers import CreateUserProfileSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+class CreateUserView(APIView):
+
+    def post(self, request):
+        req_data = request.data
+        serializer = CreateUserProfileSerializer(data=req_data)
+        serializer.is_valid(raise_exception=True)
+
+        data = serializer.validated_data
+        user_data = User(
+            username=data["username"],
+            email=data["email"],
+        )
+        user_data.set_password(data["password"])
+        user_data.save()
+
+        return Response(serializer.data)
